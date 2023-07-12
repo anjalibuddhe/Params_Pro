fda88613d0285d9fba83f94c52a0190280af2aff
anjali buddhe <anjalibuddhe124@gmail.com>
2023-07-12 16:16:57 +0530

    Add project files.

---
+++ ParamsPro/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsPro
+{
+    public class Employee
+    {
+        public void AcceptNames(params string[] names)
+        {
+            Console.WriteLine("Count " + names.Length);
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+}
---
+++ ParamsPro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsPro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee stud = new Employee();
+            stud.AcceptNames("1", "Suraj");
+            stud.AcceptNames("Kishor", "Sara", "Pankaj");
+            stud.AcceptNames("Anjali", "John", "Hari", "Gita");
+
+        }
+    }
+}
